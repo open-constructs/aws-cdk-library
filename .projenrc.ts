de803b9ec30d27538b1a68dd06ef411668414b84
Estian Yssel <estian.yssel@smartvokat.com>
2024-04-26 10:36:21 +0200

    chore: reduce minNodeVersion to `18.0.0` to be less restrictive

--- .projenrc.ts
+++ .projenrc.ts
@@ -33,7 +33,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     distName: 'open-constructs-aws-cdk',
     module: 'open_constructs_aws_cdk',
   },
-  minNodeVersion: '18.20.2',
+  minNodeVersion: '18.0.0',
   // publishToMaven: {
   //   mavenGroupId: 'org.open-constructs',
   //   mavenArtifactId: 'aws-cdk',
