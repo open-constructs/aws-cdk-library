4b83111c02fd77d888f4ed447bf2b523643ba03c
Kazuho CryerShinozuka <malaysia.cryer@gmail.com>
2024-04-29 23:31:35 +0900

    feat: implements IConnectable

--- src/aws-ec2/instance-connect-endpoint.ts
+++ src/aws-ec2/instance-connect-endpoint.ts
@@ -1,23 +1,26 @@
 import { aws_ec2 } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { Construct, IConstruct } from 'constructs';
 
-export enum InstanceConnectEndpointPort {
-  SSH = 22,
-  RDP = 3389,
+/**
+ * An EC2 Instance Connect Endpoint.
+ */
+export interface IInstanceConnectEndpoint extends aws_ec2.IConnectable, IConstruct {
+  /**
+   * The ID of the EC2 Instance Connect Endpoint.
+   *
+   * @attribute
+   */
+  readonly instanceConnectEndpointId: string;
 }
 
+/**
+ * Properties for defining an EC2 Instance Connect Endpoint.
+ */
 export interface InstanceConnectEndpointProps {
   /**
    * Unique, case-sensitive identifier that you provide to ensure the idempotency of the request.
-   *
-   * @default - Automatically generated client token.
    */
   clientToken?: string;
-
-  /**
-   * @default InstanceConnectEndpointPort.SSH
-   */
-  port?: InstanceConnectEndpointPort;
 
   /**
    * Indicates whether your client's IP address is preserved as the source.
@@ -27,27 +30,69 @@ export interface InstanceConnectEndpointProps {
   preserveClientIp?: boolean;
 
   /**
-   * The subnet in which to create the EC2 Instance Connect Endpoint.
-   */
-  subnet: aws_ec2.ISubnet;
-
-  /**
    * The security groups to associate with the EC2 Instance Connect Endpoint.
+   *
+   * @default - a new security group is created
    */
   securityGroups?: aws_ec2.ISecurityGroup[];
+
+  /**
+   * The VPC in which the EC2 Instance Connect Endpoint is created.
+   */
+  vpc: aws_ec2.IVpc;
 }
 
-export class InstanceConnectEndpoint extends Construct {
+/**
+ * Represents an EC2 Instance Connect Endpoint construct in AWS CDK.
+ *
+ * @example
+ * declare const securityGroups: aws_ec2.ISecurityGroup[];
+ * declare const vpc: aws_ec2.IVpc;
+ *
+ * const instanceConnectEndpoint = new InstanceConnectEndpoint(
+ *   stack,
+ *   'InstanceConnectEndpoint',
+ *   {
+ *     clientToken: 'my-client-token',
+ *     preserveClientIp: true,
+ *     securityGroups,
+ *     vpc,
+ *   },
+ * );
+ */
+export class InstanceConnectEndpoint extends Construct implements IInstanceConnectEndpoint {
+
+  /**
+   * The ID of the EC2 Instance Connect Endpoint.
+   */
+  public readonly instanceConnectEndpointId: string;
+
+  /**
+   * The connection object associated with the EC2 Instance Connect Endpoint.
+   */
+  public readonly connections: aws_ec2.Connections;
 
   constructor(scope: Construct, id: string, props: InstanceConnectEndpointProps) {
     super(scope, id);
 
+    const securityGroups = props.securityGroups ?? [
+      new aws_ec2.SecurityGroup(this, 'SecurityGroup', {
+        vpc: props.vpc,
+      }),
+    ];
+
+    this.connections = new aws_ec2.Connections({
+      securityGroups,
+    });
+
     const instanceConnectEndpoint = this.createInstanceConnectEndpoint(this, 'Resource', {
       clientToken: props.clientToken,
       preserveClientIp: props.preserveClientIp,
-      securityGroupIds: props.securityGroups?.map(sg => sg.securityGroupId) ?? [],
-      subnetId: props.subnet.subnetId,
+      securityGroupIds: securityGroups.map(sg => sg.securityGroupId),
+      subnetId: props.vpc.selectSubnets().subnetIds[0],
     });
+
+    this.instanceConnectEndpointId = instanceConnectEndpoint.getAtt('Id').toString();
   }
 
   protected createInstanceConnectEndpoint(
