403b371dd0069d38fbd2b607ed6eae84805f68c3
WinterYukky <49480575+WinterYukky@users.noreply.github.com>
2024-05-27 16:06:02 +0000

    ci: automate upgrade AWS CDK

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,5 @@
 import { ReleasableCommits, awscdk, github, javascript, release } from 'projen';
+import { JobPermission } from 'projen/lib/github/workflows-model';
 import { NodePackageManager } from 'projen/lib/javascript';
 
 let cdkVersion = '2.120.0';
@@ -62,6 +63,54 @@ project.addTask('integ:update', {
   exec: 'integ-runner --update-on-failed',
   description: 'Run integration tests and update on any failed tests',
   receiveArgs: true,
+});
+
+const workflow = project.github!.addWorkflow('upgrade-aws-cdk');
+workflow.on({
+  schedule: [{ cron: '0 0 * * *' }],
+  workflowDispatch: {},
+});
+workflow.addJob('create-pr', {
+  permissions: {
+    contents: JobPermission.WRITE,
+    pullRequests: JobPermission.WRITE,
+  },
+  runsOn: ['ubuntu-latest'],
+  steps: [
+    {
+      uses: 'actions/checkout@v4',
+    },
+    {
+      name: 'Install dependencies',
+      run: 'yarn install',
+    },
+    {
+      name: 'Get latest AWS CDK version',
+      run: "echo aws_cdk_version=`curl -s https://api.github.com/repos/aws/aws-cdk/releases/latest  | jq -r .tag_name | sed 's/v//'` >> $GITHUB_ENV",
+    },
+    {
+      name: 'Overwrite the AWS CDK version in.projenrc.ts with the latest',
+      run: 'sed -i "s/cdkVersion\\s=\\s\'[0-9|\\.]*\'/cdkVersion = \'${{ env.aws_cdk_version }}\'/" .projenrc.ts',
+    },
+    {
+      name: 'Synth project',
+      run: 'yarn projen',
+      env: { CI: 'false' },
+    },
+    {
+      name: 'Create Pull Request',
+      uses: 'peter-evans/create-pull-request@v6',
+      with: {
+        'commit-message':
+          'chore(deps): upgrade AWS CDK to v${{ env.aws_cdk_version }}',
+        'title': 'chore(deps): upgrade AWS CDK to v${{ env.aws_cdk_version }}',
+        'body': 'Upgrade project AWS CDK version.',
+        'base': 'main',
+        'branch': 'github-actions/upgrade-aws-cdk',
+        'delete-branch': true,
+      },
+    },
+  ],
 });
 
 project.synth();
