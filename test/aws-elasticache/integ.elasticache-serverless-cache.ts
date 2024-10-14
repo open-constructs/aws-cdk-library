fc95fd3362dcebd64122b1bd7802d7f5d1f6a6cc
maz <yiyth.fcb6@gmail.com>
2024-10-14 18:33:19 +0900

    update integ

--- test/aws-elasticache/integ.elasticache-serverless-cache.ts
+++ test/aws-elasticache/integ.elasticache-serverless-cache.ts
@@ -2,27 +2,27 @@ import { IntegTest } from '@aws-cdk/integ-tests-alpha';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ocf from '../../src';
-import { AuthenticationType, DailySnapshotTime, Engine } from '../../src/aws-elasticache';
+import { AuthenticationType, DailySnapshotTime, Engine, MajorVersion } from '../../src/aws-elasticache';
 
 class ElastiCacheStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const iamUser = new ocf.aws_elasticache.User(this, 'IamUser', {
-      accessString: 'off -@all',
+      accessString: 'on ~* +@all',
       authenticationType: AuthenticationType.IAM,
     });
 
     const noPasswordRequiredUser = new ocf.aws_elasticache.User(this, 'NoPasswordRequiredUser', {
       userId: 'no-password-required-user',
-      accessString: 'off -@all',
+      accessString: 'on ~* +@all',
       authenticationType: AuthenticationType.NO_PASSWORD_REQUIRED,
     });
 
     const passwordUser = new ocf.aws_elasticache.User(this, 'PasswordUser', {
-      userId: 'password-user-id',
+      userId: 'password-user',
       userName: 'password-user-names',
-      accessString: 'off -@all',
+      accessString: 'on ~* +@all',
       authenticationType: AuthenticationType.PASSWORD,
       passwords: [
         cdk.SecretValue.unsafePlainText('adminUserPassword123'),
@@ -39,13 +39,28 @@ class ElastiCacheStack extends cdk.Stack {
 
     const vpc = new cdk.aws_ec2.Vpc(this, 'VPC', {});
 
+    const key = new cdk.aws_kms.Key(this, 'Key', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
     new ocf.aws_elasticache.ServerlessCache(this, 'ElastiCacheServerlessCluster', {
       engine: Engine.VALKEY,
       serverlessCacheName: 'my-serverless-cache',
       dailySnapshotTime: new DailySnapshotTime({ hour: 12, minute: 0 }),
       description: 'my serverless cache',
-      finalSnapshotName: 'my-final-snapshot',
+      finalSnapshotName: 'my-finalsnapshot',
+      kmsKey: key,
+      majorEngineVersion: MajorVersion.VER_7,
+      snapshotRetentionLimit: 6,
+      securityGroups: [
+        new cdk.aws_ec2.SecurityGroup(this, 'SecurityGroup', {
+          vpc,
+        }),
+      ],
       vpc,
+      vpcSubnets: vpc.selectSubnets({
+        subnetType: cdk.aws_ec2.SubnetType.PRIVATE_WITH_EGRESS,
+      }),
       userGroup,
     });
   }
