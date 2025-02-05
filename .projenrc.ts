59b284fa679e50fd95416f9dddd00f793a8cec6e
Thorsten Hoeger <thorsten.hoeger@taimos.de>
2025-02-05 11:51:46 +0100

    chore: upgrade dependencies to latest minor versions
    - Upgrade ESLint, and related dependencies
    - Update projen, jsii, and other development tools

--- .projenrc.ts
+++ .projenrc.ts
@@ -7,7 +7,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorAddress: 'thorsten.hoeger@taimos.de',
   cdkVersion: cdkVersion,
   defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.3.0',
+  jsiiVersion: '~5.7.0',
   constructsVersion: '10.3.0',
   name: '@open-constructs/aws-cdk',
   projenrcTs: true,
