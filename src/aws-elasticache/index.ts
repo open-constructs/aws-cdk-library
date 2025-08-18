9b2bb005c0fa898711c1bcf72968ef72778afc56
go-to-k <24818752+go-to-k@users.noreply.github.com>
2025-08-19 01:22:09 +0900

    export

--- src/aws-elasticache/index.ts
+++ src/aws-elasticache/index.ts
@@ -1,5 +1,6 @@
 export * from './daily-snapshot-time';
 export * from './serverless-cache';
 export * from './util';
+export * from './serverless-cache-engine';
 export * from './user';
 export * from './user-group';
